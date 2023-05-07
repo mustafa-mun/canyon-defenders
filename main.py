1a46917d5ec7e4ada91c86c9167fcdca5f8490d5
mustafa <mustafamcm9@gmail.com>
2023-05-07 17:46:07 +0300

    update main

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from modules.background import Background
 from modules.window import Window
 from modules.enemy import Enemy
+from modules.tower import Tower
+from modules.player import Player
 import pygame
-import math
 
 pygame.init()
 
@@ -15,10 +16,28 @@ pygame.display.set_caption("Canyon Defenders")
 # create background
 background = Background("./assets/map.png", [0, 0])
 
+# create player
+player = Player()
+
 # Create a custom event for adding a new enemy every second
 ADDENEMY = pygame.USEREVENT + 1
 pygame.time.set_timer(ADDENEMY, 1000)
 
+# this will go into relative class or module
+# check if tile coordinate in a placement tile range
+def is_coordinate_valid_placement_point(x, y, coordinates):
+    for block in coordinates:
+        # get maximum coordinates
+        max_x = block["x"] + block["width"]
+        max_y = block["y"] + block["height"]
+        # check if coordinates are in range
+        if x >= block["x"] and x <= max_x and y >= block["y"] and y <= max_y:
+            # coordinates are in range
+            return block
+    # coordinates are out of range
+    print("Out ouf range")
+    return False
+   
 # main game loop
 running = True
 while running:
@@ -33,6 +52,22 @@ while running:
             win.all_sprites.add(new_enemy)
             win.enemies.add(new_enemy)
 
+        elif event.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+            valid_coordinate = is_coordinate_valid_placement_point(pos[0], pos[1], background._placement_blocks)
+            if valid_coordinate:
+                x = valid_coordinate["x"] + (valid_coordinate["width"] / 2)
+                y = valid_coordinate["y"] + (valid_coordinate["height"] / 2)
+ 
+                new_tower = Tower(30, valid_coordinate["width"], valid_coordinate["height"], x, y)
+                win.all_sprites.add(new_tower)
+                win.towers.add(new_tower)
+   
+    # handle when player out of health
+    if player.health <= 0:
+        pass
+        #running = False
+    
     # add background
     screen.blit(background._image, background._rect)
 
@@ -40,11 +75,13 @@ while running:
     for entity in win.all_sprites:
         screen.blit(entity._surf, entity._rect)
 
-    # make enemies move
-    win.enemies.update()
+    # draw players health
+    screen.blit(player.show_health(), (win._SCREEN_WIDTH - 75, 50))
 
+    # make enemies move and draw their health
     for enemy in win.enemies:
         enemy.draw_health(screen, win)
+        enemy.update(player)
 
     # Flip the display
     pygame.display.flip()
