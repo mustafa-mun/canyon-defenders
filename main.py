8163626b42d0c0d39fe38f5e97abf91d4d295d86
mustafa <mustafamcm9@gmail.com>
2023-05-07 23:56:21 +0300

    update main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from modules.background import Background
 from modules.window import Window
 from modules.enemy import Enemy
-from modules.tower import Tower
+from modules.tower import Tower, Projectile
 from modules.player import Player
 import pygame
 
@@ -22,6 +22,10 @@ player = Player()
 # Create a custom event for adding a new enemy every second
 ADDENEMY = pygame.USEREVENT + 1
 pygame.time.set_timer(ADDENEMY, 1000)
+
+# Create a custom event for shooting projectiles every second
+SHOOTPROJECTILE = pygame.USEREVENT + 2
+pygame.time.set_timer(SHOOTPROJECTILE, 1000)
 
 # this will go into relative class or module
 # check if tile coordinate in a placement tile range
@@ -51,30 +55,45 @@ while running:
             new_enemy = Enemy(100, background._waypoints)
             win.all_sprites.add(new_enemy)
             win.enemies.add(new_enemy)
-
+        # handle tower placing
         elif event.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
             valid_coordinate = is_coordinate_valid_placement_point(pos[0], pos[1], background._placement_blocks)
             if valid_coordinate:
                 x = valid_coordinate["x"] + (valid_coordinate["width"] / 2)
                 y = valid_coordinate["y"] + (valid_coordinate["height"] / 2)
- 
-                new_tower = Tower(30, valid_coordinate["width"], valid_coordinate["height"], x, y)
+                new_tower = Tower(250,valid_coordinate["width"], valid_coordinate["height"], x, y)
                 win.all_sprites.add(new_tower)
                 win.towers.add(new_tower)
+
+        # handle projectile shooting
+        elif event.type == SHOOTPROJECTILE:
+            for tower in win.towers:
+                new_projectile = Projectile(tower)
+                win.all_sprites.add(new_projectile)
+                win.projectiles.add(new_projectile)
+        
    
     # handle when player out of health
     if player.health <= 0:
         pass
         #running = False
     
+
     # add background
     screen.blit(background._image, background._rect)
 
     # draw all sprites
     for entity in win.all_sprites:
-        screen.blit(entity._surf, entity._rect)
-
+        screen.blit(entity._surf, entity.rect)
+        
+    # draw towers range
+    for tower in win.towers:
+        tower.draw_range_box(screen)
+        for enemy in win.enemies:
+            if tower.range_box.colliderect(enemy.rect):
+                # handle shooting enemies
+                pass
     # draw players health
     screen.blit(player.show_health(), (win._SCREEN_WIDTH - 75, 50))
 
@@ -83,6 +102,10 @@ while running:
         enemy.draw_health(screen, win)
         enemy.update(player)
 
+    # make projectiles move
+    for projectile in win.projectiles:
+        projectile.update(win)
+    
     # Flip the display
     pygame.display.flip()
 
