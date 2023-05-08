ac7f68b7c03b39d7adbf429c1cfe5accef561d39
mustafa <mustafamcm9@gmail.com>
2023-05-08 13:08:53 +0300

    update main

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from modules.background import Background
-from modules.window import Window
+from modules.window import Window, Background, CoordinateManager
 from modules.enemy import Enemy
 from modules.tower import Tower, Projectile
 from modules.player import Player
@@ -19,6 +18,9 @@ background = Background("./assets/map.png", [0, 0])
 # create player
 player = Player()
 
+# create coordinate manager
+coordinate_manager = CoordinateManager()
+
 # Create a custom event for adding a new enemy every second
 ADDENEMY = pygame.USEREVENT + 1
 pygame.time.set_timer(ADDENEMY, 1000)
@@ -27,20 +29,6 @@ pygame.time.set_timer(ADDENEMY, 1000)
 SHOOTPROJECTILE = pygame.USEREVENT + 2
 pygame.time.set_timer(SHOOTPROJECTILE, 1000)
 
-# this will go into relative class or module
-# check if tile coordinate in a placement tile range
-def is_coordinate_valid_placement_point(x, y, coordinates):
-    for block in coordinates:
-        # get maximum coordinates
-        max_x = block["x"] + block["width"]
-        max_y = block["y"] + block["height"]
-        # check if coordinates are in range
-        if x >= block["x"] and x <= max_x and y >= block["y"] and y <= max_y:
-            # coordinates are in range
-            return block
-    # coordinates are out of range
-    print("Out ouf range")
-    return False
    
 # main game loop
 running = True
@@ -52,13 +40,13 @@ while running:
             running = False
         # handle enemy spawn
         elif event.type == ADDENEMY:
-            new_enemy = Enemy(100, background._waypoints)
+            new_enemy = Enemy(100, coordinate_manager._waypoints)
             win.all_sprites.add(new_enemy)
             win.enemies.add(new_enemy)
         # handle tower placing
         elif event.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
-            valid_coordinate = is_coordinate_valid_placement_point(pos[0], pos[1], background._placement_blocks)
+            valid_coordinate = coordinate_manager.is_coordinate_valid_placement_point(pos[0], pos[1], coordinate_manager._placement_blocks)
             if valid_coordinate:
                 x = valid_coordinate["x"] + (valid_coordinate["width"] / 2)
                 y = valid_coordinate["y"] + (valid_coordinate["height"] / 2)
